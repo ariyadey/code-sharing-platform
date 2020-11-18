d61a5859e557dc20f4e9e94c7eec4f208d9a259e
ariya-mms <ariya.mms@gmail.com>
2020-11-18 16:36:42 +0330

    Add Freemarker templates

--- Code Sharing Platform/task/src/platform/controller/CodeController.java
+++ Code Sharing Platform/task/src/platform/controller/CodeController.java
@@ -1,6 +1,5 @@
 package platform.controller;
 
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 import platform.model.Code;
