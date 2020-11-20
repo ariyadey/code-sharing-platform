33fb305f0ca2b332618f3f39bb4e178cc103eead
ariya-mms <ariya.mms@gmail.com>
2020-11-20 17:58:06 +0330

    Optimize imports in CodeController.java

--- Code Sharing Platform/task/src/platform/controller/CodeController.java
+++ Code Sharing Platform/task/src/platform/controller/CodeController.java
@@ -2,11 +2,8 @@ package platform.controller;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.ResponseBody;
-import platform.model.CodeRepository;
+import org.springframework.web.bind.annotation.*;
+import platform.model.Code;
 
 import javax.servlet.http.HttpServletResponse;
 
