37a80c160b6a915112c296582579b259715e7c2f
ariya-mms <ariya.mms@gmail.com>
2020-11-20 18:00:21 +0330

    Add an ArrayList of Code objects
    Now, the app can hold a list of Code objects not just one!

--- Code Sharing Platform/task/src/platform/controller/CodeController.java
+++ Code Sharing Platform/task/src/platform/controller/CodeController.java
@@ -6,32 +6,35 @@ import org.springframework.web.bind.annotation.*;
 import platform.model.Code;
 
 import javax.servlet.http.HttpServletResponse;
+import java.util.ArrayList;
+import java.util.List;
 
 @Controller
 public class CodeController {
-    private static final String INITIAL_CODE_SNIPPET = "println(\"Hello World\")";
-    private final CodeRepository codeRepository = new CodeRepository(INITIAL_CODE_SNIPPET);
+    //    private static final String INITIAL_CODE_SNIPPET = "println(\"Hello World\")"; //todo maybe we don't need it anymore
+//    private final Code code = new Code(INITIAL_CODE_SNIPPET);
+    private static final List<Code> CODES = new ArrayList<>();
 
     @ResponseBody
-    @GetMapping(value = "/api/code")
-    private CodeRepository getCode(HttpServletResponse response) {
+    @GetMapping(value = "/api/code/{codeIndex}")
+    private Code getCode(HttpServletResponse response, @PathVariable int codeIndex) { //todo does it accept int?
         response.addHeader("Content-Type", "application/json");
-        return codeRepository;
+        return CODES.get(codeIndex);
     }
 
     @ResponseBody
     @PostMapping(value = "/api/code/new", consumes = "application/json")
-    private String postCode(@RequestBody CodeRepository codeRepository, HttpServletResponse response) {
+    private String postCode(@RequestBody Code code, HttpServletResponse response) {
         response.addHeader("Content-Type", "application/json");
-        this.codeRepository.update(codeRepository.getCode());
-        return "{}";
+        CODES.add(code);
+        return String.format("{\"%s\": \"%d\"}", "id", CODES.size() - 1); //todo does it work?
     }
 
-    @GetMapping(value = "/code")
-    private String getCodeView(HttpServletResponse response, Model model) {
+    @GetMapping(value = "/code/{codeNumber}")   //todo does it accept int?
+    private String getCodeView(HttpServletResponse response, Model model, @PathVariable int codeNumber) {
         response.addHeader("Content-Type", "text/html");
-        model.addAttribute("date", codeRepository.getDate());
-        model.addAttribute("code", codeRepository.getCode());
+        model.addAttribute("date", CODES.get(codeNumber).getDate());
+        model.addAttribute("code", CODES.get(codeNumber).getCode());
         return "code";
     }
 
