8689e02695b500c7d1dcf2557f5dcbd37b3a6ddf
ariya-mms <ariya.mms@gmail.com>
2020-11-21 14:57:32 +0330

    Improve code reliability of returning correct order of objects

--- Code Sharing Platform/task/src/platform/model/CodeRepo.java
+++ Code Sharing Platform/task/src/platform/model/CodeRepo.java
@@ -3,7 +3,7 @@ package platform.model;
 import org.springframework.stereotype.Component;
 
 import java.util.ArrayList;
-import java.util.Comparator;
+import java.util.Collections;
 import java.util.List;
 
 @Component
@@ -23,11 +23,11 @@ public class CodeRepo {
         return codes.size();
     }
 
-    //todo improve this method
     public List<Code> getLatest(int n) {
         final var size = codes.size();
         final var latestCodes = new ArrayList<>(codes.subList(size > n ? size - n : 0, size));
-        latestCodes.sort(Comparator.comparing(Code::getDate).reversed());
+//        latestCodes.sort(Comparator.comparing(Code::getDate).reversed());
+        Collections.reverse(latestCodes);
         return latestCodes;
     }
 }
