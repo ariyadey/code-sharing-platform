87781410a78d164714316800304067242ef3091d
ariya-mms <ariya.mms@gmail.com>
2020-11-21 14:28:54 +0330

    Remove some done TODOs

--- Code Sharing Platform/task/src/platform/controller/APIResponseHeaderFilter.java
+++ Code Sharing Platform/task/src/platform/controller/APIResponseHeaderFilter.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 //@WebFilter("/api/*")
-@Component("/api/*")    //todo does it work?
+@Component("/api/*")
 public class APIResponseHeaderFilter implements Filter {
 
     @Override
--- Code Sharing Platform/task/src/platform/controller/ViewResponseHeaderFilter.java
+++ Code Sharing Platform/task/src/platform/controller/ViewResponseHeaderFilter.java
@@ -7,7 +7,7 @@ import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 //@WebFilter("")
-@Component()    //todo does it work?
+@Component()
 public class ViewResponseHeaderFilter implements Filter {
 
     @Override
