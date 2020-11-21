01c989c1611e67e9126ce3a88e5711c7bc8cbeb9
ariya-mms <ariya.mms@gmail.com>
2020-11-21 13:00:22 +0330

    Add repo field
    Now, it's possible to remove residual dependency

--- Code Sharing Platform/task/src/platform/controller/APIController.java
+++ Code Sharing Platform/task/src/platform/controller/APIController.java
@@ -1,25 +1,31 @@
 package platform.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 import platform.model.Code;
+import platform.model.CodeRepo;
 
 import java.util.ArrayList;
 import java.util.Comparator;
 import java.util.List;
 
-import static platform.CodeSharingPlatform.CODES;
-
 @RestController
 public class APIController {
+    private final CodeRepo repo;
+
+    @Autowired
+    public APIController(CodeRepo repo) {
+        this.repo = repo;
+    }
 
     @GetMapping(value = "/api/code/{codeIndex}")
     private Code getCode(@PathVariable int codeIndex) { //todo does it accept int?
-        return CODES.get(codeIndex - 1);
+        return repo.getCodes().get(codeIndex - 1);
     }
 
     @GetMapping(value = "/api/code/latest")
     private List<Code> getLatestCode() {
-        final var latestCodes = new ArrayList<>(CODES.subList(CODES.size() > 10 ? CODES.size() - 10 : 0, CODES.size()));
+        final var latestCodes = new ArrayList<>(repo.getCodes().subList(repo.getCodes().size() > 10 ? repo.getCodes().size() - 10 : 0, repo.getCodes().size()));
         latestCodes.sort(Comparator.comparing(Code::getDate).reversed());   //todo extract method
         return latestCodes;
     }
@@ -27,8 +33,8 @@ public class APIController {
     @PostMapping(value = "/api/code/new", consumes = "application/json")
     private String postCode(@RequestBody Code code) {
         code.update();
-        CODES.add(code);
+        repo.getCodes().add(code);
         //todo return in a more modern way
-        return String.format("{\"%s\": \"%d\"}", "id", CODES.size());
+        return String.format("{\"%s\": \"%d\"}", "id", repo.getCodes().size());
     }
 }
