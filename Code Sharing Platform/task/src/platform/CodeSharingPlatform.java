d771e7f78306a87bcb613c014d05f1d24feded70
ariya-mms <ariya.mms@gmail.com>
2020-11-25 11:44:20 +0330

    Delete runApplication method

--- Code Sharing Platform/task/src/platform/CodeSharingPlatform.java
+++ Code Sharing Platform/task/src/platform/CodeSharingPlatform.java
@@ -1,10 +1,7 @@
 package platform;
 
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import platform.model.CodeRepository;
 
 
 @SpringBootApplication
@@ -15,15 +12,14 @@ public class CodeSharingPlatform {
         SpringApplication.run(CodeSharingPlatform.class, args);
     }
 
-    @Bean
-    public CommandLineRunner runApplication(CodeRepository repository) {
-        return args -> {
-            
-        };
-    }
+//    @Bean
+//    public CommandLineRunner runApplication(CodeRepository repository) {
+//        return args -> {
+//
+//        };
+//    }
 }
 
 /*
 todo: change the hierarchy of folders according to your specifications and mvc
-todo: format the output dateTime
 */
