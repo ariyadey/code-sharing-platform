b409a34c3eb54ae8497c57882625c97ee3a32696
ariya-mms <ariya.mms@gmail.com>
2020-11-25 11:45:53 +0330

    Add findAllOrderByDateDesc method

--- Code Sharing Platform/task/src/platform/model/CodeRepository.java
+++ Code Sharing Platform/task/src/platform/model/CodeRepository.java
@@ -1,32 +1,11 @@
 package platform.model;
 
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Component;
 
-public interface CodeRepository extends CrudRepository<Code, Integer> {
+import java.util.List;
+
+@Component
+public interface CodeRepository extends CrudRepository<Code, Long> {
+    List<Code> findAllOrderByDateDesc();
 }
-
-//@Component
-//public final class CodeRepository {
-//    private final List<Code> codes = new ArrayList<>();
-//
-//    public final Code getAt(int index) {
-//        return codes.get(index);
-//    }
-//
-//    public final void add(Code code) {
-//        code.update();
-//        codes.add(code);
-//    }
-//
-//    public final int size() {
-//        return codes.size();
-//    }
-//
-//    public final List<Code> getLatest(int n) {
-//        final var size = codes.size();
-//        final var latestCodes = new ArrayList<>(codes.subList(size > n ? size - n : 0, size));
-////        latestCodes.sort(Comparator.comparing(Code::getDate).reversed());
-//        Collections.reverse(latestCodes);
-//        return latestCodes;
-//    }
-//}
