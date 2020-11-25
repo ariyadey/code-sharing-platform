cf1d69df0138d948fdfb84d590936e5439fb48bd
ariya-mms <ariya.mms@gmail.com>
2020-11-25 22:48:43 +0330

    Change package name from utils to util

--- Code Sharing Platform/task/src/platform/controller/APIController.java
+++ Code Sharing Platform/task/src/platform/controller/APIController.java
@@ -6,7 +6,6 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.server.ResponseStatusException;
 import platform.model.Code;
 import platform.model.CodeRepository;
-import platform.utils.DateTime;
 
 import java.time.LocalDateTime;
 import java.time.ZoneId;
--- Code Sharing Platform/task/src/platform/util/DateTime.java
+++ Code Sharing Platform/task/src/platform/util/DateTime.java
@@ -1,4 +1,4 @@
-package platform.utils;
+package platform.util;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
