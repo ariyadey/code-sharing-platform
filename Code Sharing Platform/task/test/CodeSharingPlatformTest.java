17ada639054a7c4b47ab5a648c9f1af48a107c83
ariya-mms <ariya.mms@gmail.com>
2020-11-25 15:38:50 +0330

    Update CodeSharingPlatformTest.java. to test Stage 4

--- Code Sharing Platform/task/test/CodeSharingPlatformTest.java
+++ Code Sharing Platform/task/test/CodeSharingPlatformTest.java
@@ -16,7 +16,9 @@ import java.util.HashMap;
 import java.util.Map;
 
 import static org.hyperskill.hstest.testing.expect.Expectation.expect;
-import static org.hyperskill.hstest.testing.expect.json.JsonChecker.*;
+import static org.hyperskill.hstest.testing.expect.json.JsonChecker.isArray;
+import static org.hyperskill.hstest.testing.expect.json.JsonChecker.isObject;
+import static org.hyperskill.hstest.testing.expect.json.JsonChecker.isString;
 
 public class CodeSharingPlatformTest extends SpringTest {
     public CodeSharingPlatformTest() {
@@ -33,93 +35,24 @@ public class CodeSharingPlatformTest extends SpringTest {
     final String WEB_LATEST = "/code/latest";
 
     final String[] SNIPPETS = {
-            "public static void ...",
-            "class Code { ...",
-            "Snippet #3",
-            "Snippet #4",
-            "Snippet #5",
-            "Snippet #6",
-            "Snippet #7",
-            "Snippet #8",
-            "Snippet #9",
-            "Snippet #10",
-            "Snippet #11",
-            "Snippet #12",
-            "Snippet #13",
-            "Snippet #14",
+        "public static void ...",
+        "class Code { ...",
+        "Snippet #3",
+        "Snippet #4",
+        "Snippet #5",
+        "Snippet #6",
+        "Snippet #7",
+        "Snippet #8",
+        "Snippet #9",
+        "Snippet #10",
+        "Snippet #11",
+        "Snippet #12",
+        "Snippet #13",
+        "Snippet #14",
     };
 
     final Map<Integer, String> ids = new HashMap<>();
     final Map<Integer, String> dates = new HashMap<>();
-    @DynamicTestingMethod
-    public DynamicTesting[] dt = new DynamicTesting[]{
-            // test 1
-            this::checkWebCodeNew,
-
-            // test 2
-            () -> postSnippet(0),
-            () -> checkApiCode(0),
-            () -> checkWebCode(0),
-            () -> checkApiLatest(0),
-            () -> checkWebLatest(0),
-
-            // test 7
-            () -> postSnippet(1),
-            () -> checkApiCode(0),
-            () -> checkWebCode(0),
-            () -> checkApiCode(1),
-            () -> checkWebCode(1),
-            () -> checkApiLatest(1, 0),
-            () -> checkWebLatest(1, 0),
-
-            // test 14
-            () -> postSnippet(2),
-            () -> postSnippet(3),
-            () -> postSnippet(4),
-            () -> postSnippet(5),
-            () -> postSnippet(6),
-            () -> postSnippet(7),
-            () -> postSnippet(8),
-            () -> postSnippet(9),
-            () -> postSnippet(10),
-            () -> postSnippet(11),
-            () -> postSnippet(12),
-            () -> postSnippet(13),
-
-            // test 26
-            () -> checkApiCode(0),
-            () -> checkWebCode(0),
-            () -> checkApiCode(1),
-            () -> checkWebCode(1),
-            () -> checkApiCode(2),
-            () -> checkWebCode(2),
-            () -> checkApiCode(3),
-            () -> checkWebCode(3),
-            () -> checkApiCode(4),
-            () -> checkWebCode(4),
-            () -> checkApiCode(5),
-            () -> checkWebCode(5),
-            () -> checkApiCode(6),
-            () -> checkWebCode(6),
-            () -> checkApiCode(7),
-            () -> checkWebCode(7),
-            () -> checkApiCode(8),
-            () -> checkWebCode(8),
-            () -> checkApiCode(9),
-            () -> checkWebCode(9),
-            () -> checkApiCode(10),
-            () -> checkWebCode(10),
-            () -> checkApiCode(11),
-            () -> checkWebCode(11),
-            () -> checkApiCode(12),
-            () -> checkWebCode(12),
-            () -> checkApiCode(13),
-            () -> checkWebCode(13),
-
-            // test 54
-            () -> checkApiLatest(13, 12, 11, 10, 9, 8, 7, 6, 5, 4),
-            () -> checkWebLatest(13, 12, 11, 10, 9, 8, 7, 6, 5, 4),
-    };
 
     static String th(int val) {
         if (val == 1) {
@@ -136,7 +69,7 @@ public class CodeSharingPlatformTest extends SpringTest {
     static void checkStatusCode(HttpResponse resp, int status) {
         if (resp.getStatusCode() != status) {
             throw new WrongAnswer(
-                    resp.getRequest().getMethod() + " " +
+                resp.getRequest().getMethod() + " " +
                     resp.getRequest().getLocalUri() +
                     " should respond with status code " + status + ", " +
                     "responded: " + resp.getStatusCode() + "\n\n" +
@@ -148,7 +81,7 @@ public class CodeSharingPlatformTest extends SpringTest {
     static void checkTitle(Document doc, String url, String title) {
         if (!doc.title().equals(title)) {
             throw new WrongAnswer("GET " + url +
-                                  " should contain title \"" + title + "\"");
+                " should contain title \"" + title + "\"");
         }
     }
 
@@ -161,8 +94,8 @@ public class CodeSharingPlatformTest extends SpringTest {
         Elements elems = doc.getElementsByTag(tag);
         if (elems.size() != length) {
             throw new WrongAnswer("GET " + url +
-                                  " should contain " + length + " <" + tag + "> " +
-                                  "element" + (length == 1 ? "" : "s") + ", found: " + elems.size());
+                " should contain " + length + " <" + tag + "> " +
+                "element"+ (length == 1 ? "" : "s") +", found: " + elems.size());
         }
         return elems;
     }
@@ -172,17 +105,87 @@ public class CodeSharingPlatformTest extends SpringTest {
 
         if (elem == null) {
             throw new WrongAnswer("GET " + url +
-                                  " should contain an element with id \"" + id + "\", no one found");
+                " should contain an element with id \"" + id + "\", no one found");
         }
 
         if (!elem.tagName().equals(tag)) {
             throw new WrongAnswer("GET " + url +
-                                  " should contain an element with id  \"" + id + "\" and tag \"" + tag + "\", " +
-                                  "found another tag: \"" + elem.tagName() + "\"");
+                " should contain an element with id  \"" + id + "\" and tag \"" + tag + "\", " +
+                "found another tag: \"" + elem.tagName() + "\"");
         }
 
         return elem;
     }
+
+    @DynamicTestingMethod
+    public DynamicTesting[] dt = new DynamicTesting[] {
+        // test 1
+        this::checkWebCodeNew,
+
+        // test 2
+        () -> postSnippet(0),
+        () -> checkApiCode(0),
+        () -> checkWebCode(0),
+        () -> checkApiLatest(0),
+        () -> checkWebLatest(0),
+
+        // test 7
+        () -> postSnippet(1),
+        () -> checkApiCode(0),
+        () -> checkWebCode(0),
+        () -> checkApiCode(1),
+        () -> checkWebCode(1),
+        () -> checkApiLatest(1, 0),
+        () -> checkWebLatest(1, 0),
+
+        // test 14
+        () -> postSnippet(2),
+        () -> postSnippet(3),
+        () -> postSnippet(4),
+        () -> postSnippet(5),
+        () -> postSnippet(6),
+        () -> postSnippet(7),
+        () -> postSnippet(8),
+        () -> postSnippet(9),
+        () -> postSnippet(10),
+        () -> postSnippet(11),
+        () -> postSnippet(12),
+        () -> postSnippet(13),
+
+        // test 26
+        () -> checkApiCode(0),
+        () -> checkWebCode(0),
+        () -> checkApiCode(1),
+        () -> checkWebCode(1),
+        () -> checkApiCode(2),
+        () -> checkWebCode(2),
+        () -> checkApiCode(3),
+        () -> checkWebCode(3),
+        () -> checkApiCode(4),
+        () -> checkWebCode(4),
+        () -> checkApiCode(5),
+        () -> checkWebCode(5),
+        () -> checkApiCode(6),
+        () -> checkWebCode(6),
+        () -> checkApiCode(7),
+        () -> checkWebCode(7),
+        () -> checkApiCode(8),
+        () -> checkWebCode(8),
+        () -> checkApiCode(9),
+        () -> checkWebCode(9),
+        () -> checkApiCode(10),
+        () -> checkWebCode(10),
+        () -> checkApiCode(11),
+        () -> checkWebCode(11),
+        () -> checkApiCode(12),
+        () -> checkWebCode(12),
+        () -> checkApiCode(13),
+        () -> checkWebCode(13),
+
+        // test 54
+        () -> checkApiLatest(13, 12, 11, 10, 9, 8, 7, 6, 5, 4),
+        () -> checkWebLatest(13, 12, 11, 10, 9, 8, 7, 6, 5, 4),
+    };
 
     private CheckResult checkApiCode(int id) {
         String codeId = ids.get(id);
@@ -192,15 +195,15 @@ public class CodeSharingPlatformTest extends SpringTest {
         checkStatusCode(resp, 200);
 
         expect(resp.getContent()).asJson().check(
-                isObject()
-                        .value("code", snippet)
-                        .value("date", isString(s -> {
-                            if (dates.containsKey(id)) {
-                                return s.equals(dates.get(id));
-                            }
-                            dates.put(id, s);
-                            return true;
-                        }))
+            isObject()
+                .value("code", snippet)
+                .value("date", isString(s -> {
+                    if (dates.containsKey(id)) {
+                        return s.equals(dates.get(id));
+                    }
+                    dates.put(id, s);
+                    return true;
+                }))
         );
 
         return CheckResult.correct();
@@ -224,14 +227,14 @@ public class CodeSharingPlatformTest extends SpringTest {
         String webSnippetCode = pre.text();
         if (!webSnippetCode.trim().equals(apiSnippet.trim())) {
             return CheckResult.wrong("Web code snippet " +
-                                     "and api code snippet are different");
+                "and api code snippet are different");
         }
 
         Element date = getById(doc, req, "load_date", "span");
         String webSnippetDate = date.text();
         if (!webSnippetDate.trim().equals(apiDate.trim())) {
             return CheckResult.wrong("Web snippet date " +
-                                     "and api snippet date are different");
+                "and api snippet date are different");
         }
 
         return CheckResult.correct();
@@ -259,16 +262,16 @@ public class CodeSharingPlatformTest extends SpringTest {
         checkStatusCode(resp, 200);
 
         expect(resp.getContent()).asJson().check(
-                isObject()
-                        .value("id", isString(i -> {
-                            try {
-                                Integer.parseInt(i);
-                            } catch (NumberFormatException ex) {
-                                return false;
-                            }
-                            ids.put(id, "" + i);
-                            return true;
-                        }))
+            isObject()
+                .value("id", isString(i -> {
+                    try {
+                        Integer.parseInt(i);
+                    } catch (NumberFormatException ex) {
+                        return false;
+                    }
+                    ids.put(id, "" + i);
+                    return true;
+                }))
         );
 
         return CheckResult.correct();
@@ -280,10 +283,10 @@ public class CodeSharingPlatformTest extends SpringTest {
         checkStatusCode(resp, 200);
 
         expect(resp.getContent()).asJson().check(
-                isArray(ids.length, isObject()
-                        .value("code", isString())
-                        .value("date", isString())
-                )
+            isArray(ids.length, isObject()
+                .value("code", isString())
+                .value("date", isString())
+            )
         );
 
         JsonArray elem = resp.getJson().getAsJsonArray();
@@ -298,16 +301,16 @@ public class CodeSharingPlatformTest extends SpringTest {
 
             if (!actualSnippet.equals(givenSnippet)) {
                 return CheckResult.wrong("GET " + req + " " + th(i + 1) +
-                                         " snippet doesn't match actual snippet.\n" +
-                                         "Expected:\n" + actualSnippet + "\n" +
-                                         "Found:\n" + givenSnippet);
+                    " snippet doesn't match actual snippet.\n" +
+                    "Expected:\n" + actualSnippet + "\n" +
+                    "Found:\n" + givenSnippet);
             }
 
             if (!actualDate.equals(givenDate)) {
                 return CheckResult.wrong("GET " + req + " " + th(i + 1) +
-                                         " snippet's date doesn't match actual snippet's date.\n" +
-                                         "Expected:\n" + actualDate + "\n" +
-                                         "Found:\n" + givenDate);
+                    " snippet's date doesn't match actual snippet's date.\n" +
+                    "Expected:\n" + actualDate + "\n" +
+                    "Found:\n" + givenDate);
             }
         }
 
@@ -335,19 +338,19 @@ public class CodeSharingPlatformTest extends SpringTest {
             String webSnippetCode = pre.text();
             if (!webSnippetCode.trim().equals(apiSnippet.trim())) {
                 return CheckResult.wrong("GET " + req + " " + th(i + 1)
-                                         + " snippet doesn't match " + th(i + 1) + " snippet via api.\n" +
-                                         "Expected:\n" + apiSnippet + "\n" +
-                                         "Found:\n" + webSnippetCode);
+                    + " snippet doesn't match " + th(i + 1) + " snippet via api.\n" +
+                    "Expected:\n" + apiSnippet + "\n" +
+                    "Found:\n" + webSnippetCode);
             }
 
             Element date = spanList.get(i);
             String webSnippetDate = date.text();
             if (!webSnippetDate.trim().equals(apiDate.trim())) {
                 return CheckResult.wrong("GET " + req + " " + th(i + 1)
-                                         + " snippet's date doesn't match " + th(i + 1)
-                                         + " snippet's date via api.\n" +
-                                         "Expected:\n" + apiDate + "\n" +
-                                         "Found:\n" + webSnippetDate);
+                    + " snippet's date doesn't match " + th(i + 1)
+                    + " snippet's date via api.\n" +
+                    "Expected:\n" + apiDate + "\n" +
+                    "Found:\n" + webSnippetDate);
             }
         }
 
