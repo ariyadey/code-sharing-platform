4fcf8bea45fe1454f46d8bb2eaf2e61b8eafbd05
ariya-mms <ariya.mms@gmail.com>
2020-11-25 19:38:24 +0330

    Update utility methods about date and time

--- Code Sharing Platform/task/src/platform/utils/DateTime.java
+++ Code Sharing Platform/task/src/platform/utils/DateTime.java
@@ -6,7 +6,7 @@ import java.time.format.DateTimeFormatter;
 public class DateTime {
     private final static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("E, yyyy MMM dd, HH:mm:ss");
 
-    public static String nowFormatted() {
-        return LocalDateTime.now().format(FORMATTER);
+    public static String Formatted(LocalDateTime dateTime) {
+        return dateTime.format(FORMATTER);
     }
 }
