3f23c8b8746bc77f15034eead7718f59a70ea117
ariya-mms <ariya.mms@gmail.com>
2020-11-25 15:28:32 +0330

    Update handler methods to be compatible with JPA

--- Code Sharing Platform/task/src/platform/controller/ViewController.java
+++ Code Sharing Platform/task/src/platform/controller/ViewController.java
@@ -1,10 +1,12 @@
 package platform.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.server.ResponseStatusException;
 import platform.model.CodeRepository;
 
 @Controller
@@ -17,17 +19,20 @@ public final class ViewController {
     }
 
     //todo can you make two statements one? (With the help of Freemarker)
-    @GetMapping(value = "/code/{codeNumber}")
-    private String getCodeView(Model model, @PathVariable int codeNumber) {
-        model.addAttribute("date", repo.getAt(codeNumber - 1).getDate());
-        model.addAttribute("code", repo.getAt(codeNumber - 1).getCode());
+    @GetMapping(value = "/code/{id}")
+    private String getCodeView(Model model, @PathVariable long id) {
+        final var code = repo.findById(id).orElseThrow(
+                () -> new ResponseStatusException(HttpStatus.NOT_FOUND, "There is no code with the given ID")
+        );
+        model.addAttribute("date", code.getDate());
+        model.addAttribute("code", code.getCode());
         return "code";
     }
 
     // todo: it returns the 10 most recently uploaded codes
     @GetMapping(value = "/code/latest")
     private String getLatestCodeView(Model model) {
-        model.addAttribute("codeList", repo.getLatest(10));
+        model.addAttribute("codeList", repo.findLatestByOrderByDateDesc(10));
         return "codes";
     }
 
