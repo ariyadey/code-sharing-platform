0a5d20db40f319b83dae81c908e394d9158dcd52
ariya-mms <ariya.mms@gmail.com>
2020-11-27 22:19:24 +0330

    Update DateTime.java

--- Code Sharing Platform/task/src/platform/util/DateTime.java
+++ Code Sharing Platform/task/src/platform/util/DateTime.java
@@ -4,6 +4,9 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 
 public class DateTime {
+
+    private DateTime() {}
+
     private final static DateTimeFormatter FORMATTER = DateTimeFormatter.ofPattern("E, yyyy MMM dd, HH:mm:ss");
 
     public static String formatted(LocalDateTime dateTime) {
