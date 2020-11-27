15ae6742763285bd83835e18c0eb6217970277af
ariya-mms <ariya.mms@gmail.com>
2020-11-27 22:18:03 +0330

    Update code repository to work with projections

--- Code Sharing Platform/task/src/platform/repository/CodeRepository.java
+++ Code Sharing Platform/task/src/platform/repository/CodeRepository.java
@@ -3,16 +3,20 @@ package platform.repository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 import platform.model.Code;
+import platform.projection.CodeProjection;
 
+import java.time.LocalDateTime;
 import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
 
-//@Component
 @Repository
 public interface CodeRepository extends CrudRepository<Code, Long> {
 
-    List<Code> findAllByOrderByDateDesc();
+    //Todo: check if it doesn't have any problem with rows less than 10
+    List<CodeProjection> findFirst10BySecretFalseOrderByUploadDateTimeDesc();
 
-    default List<Code> findLatestByOrderByDateDesc(int length) {
-        return findAllByOrderByDateDesc().subList(0, (int) Math.min(length, count()));
-    }
+    Optional<CodeProjection> findByIdAndViewsLeftGreaterThanAndExpirationDateTimeAfter(UUID uuid,
+                                                                                       int viewsLeft,
+                                                                                       LocalDateTime ExpirationDatetime);
 }
